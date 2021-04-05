48bd31037843bc84aa2f7b9e4add77f5b6512b7e
Messer <messer979@gmail.com>
2021-04-05 17:37:48 +0100

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,8 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              {/* <img src={logo} alt="PR" style={{ width: '88px' }} /> */}
+              PR Nannies
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,17 +64,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
